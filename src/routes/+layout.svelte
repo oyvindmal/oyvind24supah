93d022d33862b6a53a128ce0e71c73200a5968de
Øyvind Malin <oyvindmal@outlook.com>
2024-04-22 12:42:25 +0200

    Super basic styling

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    /** @type {import('./$types').LayoutData} */
+    //export let data;
+
+  import '../app.css'
+
+</script>
+
+<header><h1><a href="/">Min test app</a></h1></header>
+<slot />
